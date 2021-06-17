edff45833c6d115e22d7fceb27ad98740e23d9f3
preslavmihaylov <preslav@pmihaylov.com>
2021-06-17 10:35:30 +0300

    v0.2

--- ordertocubby.go
+++ ordertocubby.go
@@ -1,3 +1,4 @@
+// Package ordertocubby provides routines for mapping an order to a cubby
 package ordertocubby
 
 import (
@@ -5,9 +6,15 @@ import (
 	"strconv"
 )
 
-// Map an order by its ID to the correct cubby for that order
-func Map(orderID string, cubbiesCount uint32) string {
-	return strconv.Itoa(int(hash(orderID) % cubbiesCount))
+// Map an order by its ID to the correct cubby for that order by hashing it the specified number of times
+func Map(orderID string, times, cubbiesCount uint32) string {
+	result := hash(orderID)
+	for times > 1 {
+		result = hash(strconv.Itoa(int(result)))
+		times--
+	}
+
+	return strconv.Itoa(int(result % cubbiesCount))
 }
 
 func hash(s string) uint32 {
